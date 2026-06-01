ee62d485988a94f691124005f14f48ef030eba3a
Roman <61121402+rebrov-rc@users.noreply.github.com>
2026-06-01 22:44:02 +0300

    first commit

---
+++ src/test/java/org/softspace/userprofile/UserProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.softspace.userprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
